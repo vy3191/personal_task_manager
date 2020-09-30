b31dd01096578ff70dc0023d9280c6a6eef5de63
vy3191 <venkyafhs@gmail.com>
2020-09-30 13:59:18 -0400

    Modified some files

--- src/pages/Base/App.js
+++ src/pages/Base/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Header from './Header';
-import Footer from '../Footer/Footer';
+import Header from '../../components/Header/Header.js';
+import Footer from '../../components/Footer/Footer.js';
 
 
 class App extends React.Component {
