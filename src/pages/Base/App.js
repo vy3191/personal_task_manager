44cc3a011875731e62a2830c2283f3b70ce615c2
vy3191 <venkyafhs@gmail.com>
2020-10-01 13:59:52 -0400

    Implemented alias

--- src/pages/Base/App.js
+++ src/pages/Base/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import Header from '../../components/Header/Header';
-import Footer from '../../components/Footer/Footer';
+import {Header, Footer } from 'components';
+// import Header from '../../components/Header/Header';
+// import Footer from '../../components/Footer/Footer';
 
 
 class App extends React.Component {
