5c953c9b0e69cf19eabe30d741073f950ead1955
vy3191 <venkyafhs@gmail.com>
2020-10-02 12:24:09 -0400

    Added production/development mode dynamically

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,35 @@
 const path = require('path');
+// webpack config should be return as a function or an object
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  resolve: {
-    extensions: [ '.jsx', '.js', '.json', '.css', '.scss'],
-    alias: {
-      components: path.resolve(__dirname, 'src/components/'),
-      pages: path.resolve(__dirname, 'src/pages/')
-    }
-  },
-  module: {
-    rules: [
-      { 
-        test: /\.jsx?$/, 
-        exclude: /node_modules/,
-        use: 'babel-loader' 
-      }
-    ]
-  },
-  mode: 'development'
 
-};
+module.exports = (env) => {  
+    const isProd = env && env.production;
+    console.log(env)
+    return {
+      // watch: isProd && false || true,   Same code using the negation
+      watch: !isProd,
+      entry: './src/index.js',
+      output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+      },
+      resolve: {
+        extensions: [ '.jsx', '.js', '.json', '.css', '.scss'],
+        alias: {
+          components: path.resolve(__dirname, 'src/components/'),
+          pages: path.resolve(__dirname, 'src/pages/')
+        }
+      },
+      module: {
+        rules: [
+          { 
+            test: /\.jsx?$/, 
+            exclude: /node_modules/,
+            use: 'babel-loader' 
+          }
+        ]
+      },
+      mode: isProd && 'production' || 'development'
+    
+    };
+}
