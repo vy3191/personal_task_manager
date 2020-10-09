dadf7dad5bd9a409796fd0565239e3a18b1dae9f
vy3191 <venkyafhs@gmail.com>
2020-10-09 09:59:13 -0400

    Added a card component

---
+++ src/components/Card/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Card extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Card content goes here.</h2>
+      </div>
+    );
+  }
+}
+
+export default Card;
--- src/components/index.js
+++ src/components/index.js
@@ -7,4 +7,5 @@
 
 export {default as Header} from './Header/Header';
 export {default as Button } from './Button';
+export {default as Card } from './Card';
 // export {default as Footer} from './Footer/Footer';
