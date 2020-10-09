847ce903da935c25a417a1ddc1b343f3adea854d
vy3191 <venkyafhs@gmail.com>
2020-10-09 13:57:33 -0400

    Added an image and modified folders in public

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Card extends Component {
   render() {
+    const {imgAlt, imgScr, title} = this.props;
     return (
-      <div>
-        <h2>Card content goes here.</h2>
-      </div>
+      <article className="components-card">
+        <p className={title}>{title}</p>
+        {imgScr && <img src={imgScr} alt={imgAlt} />}
+      </article>
     );
   }
+}
+
+Card.defaultProps = {
+  imgAlt:''
+}
+
+Card.propTypes = {
+  imgAlt: PropTypes.string,
+  imgScr: PropTypes.string,
+  title: PropTypes.string.isRequired
 }
 
 export default Card;
